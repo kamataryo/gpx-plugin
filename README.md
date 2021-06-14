247d1da278937023dd84b2e4e6b9e36b6723617d
Kamata, Ryo <kamataryo@users.noreply.github.com>
2021-06-14 17:26:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Geolonia Plugin Boilerplate
+
+Geolonia [Embed API](https://docs.geolonia.com/embed-api/) のボイラープレートです。
+
+## 使い方
+
+```html
+<div class="geolonia"></div>
+<script src="https://cdn.geolonia.com/v1/embed?geolonia-api-key=YOUR-API-KEY"></script>
+<script src="https://geolonia.github.io/plugin-boilerplate/hello-geolonia-plugin.min.js>
+```
+
+## コントリビューション
+
+Issue、プルリクエストはいつでも歓迎します。
+以下のコマンドで開発用サーバーを立ち上げることができます。
+
+```shell
+$ git clone git@github.com:geolonia/plugin-boilerplate.git
+$ cd plugin-boilerplate
+$ yarn # または npm install
+$ npm start
+```
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+
+export const assets = path.resolve(__dirname, 'public')
+export const docs = path.resolve(__dirname, "docs")
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(assets, 'index.html') }),
+  ],
+  devtool: 'source-map',
+  output: {
+    path: docs,
+    filename: "hello-geolonia-plugin.min.js",
+  }
+};
+
+export default config
